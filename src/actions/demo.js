86d59c9a73fbde49f59fa47bfd9e9bd100d6480a
dh <441686961@qq.com>
2019-05-13 10:35:30 +0800

    init

--- src/actions/demo.js
+++ src/actions/demo.js
@@ -1,13 +1,12 @@
 /***
  * ACTIONS
  */
-import http from '../service/api'
-
-export default http
+import http from '../service/http'
+import * as api from '../const/api'
 
 export const getId = async payload => {
   console.log('payload', payload)
-  let result = await http.get('https://portal.weiwuu.com/id')
+  let result = await http.get(api.CREATE_ID)
   console.log('result2', result)
   return result.data.body.data
 }
---
+++ src/const/api.js
@@ -0,0 +1,5 @@
+/***
+ * 接口
+ */
+
+export const CREATE_ID = 'https://portal.weiwuu.com/id'
